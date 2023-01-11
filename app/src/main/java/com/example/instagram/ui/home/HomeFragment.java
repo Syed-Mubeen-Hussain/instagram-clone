b9a6661dfc40f1d05dd42698156c473f92d8954d
Syed Mubeen Hussain <mubeen1x1@gmail.com>
2023-01-11 18:52:36 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/instagram/ui/home/HomeFragment.java
@@ -0,0 +1,27 @@
+package com.example.instagram.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.arch.lifecycle.ViewModelProvider;
+
+import com.example.instagram.R;
+import com.example.instagram.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View root = inflater.inflate(R.layout.fragment_home, container ,false);
+
+        return root;
+    }
+}
